4bad46b287675ca746ce61cf2de0130878d59abb
tw.jang <tw.jang@dandisoft.co.kr>
2020-01-04 17:09:09 +0900

    [기타] 1. init project.

---
+++ src/main/java/kr/co/dandisoft/oauth2cookbook/Oauth2CookbookApplication.java
@@ -0,0 +1,13 @@
+package kr.co.dandisoft.oauth2cookbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2CookbookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2CookbookApplication.class, args);
+    }
+
+}
